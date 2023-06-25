67210cf3c7feeaa26ecda870c73e0a23c19b1989
abel.agbachi <agbachiabel@gmail.com>
2023-06-25 07:42:53 +0100

    added exception handling for not found errors

--- src/main/java/com/abel/countriesapi/exception/ErrorHandler.java
+++ src/main/java/com/abel/countriesapi/exception/ErrorHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -18,11 +19,25 @@ import java.util.Date;
 public class ErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ClassCastException.class)
-    public final ResponseEntity<AppResponse<ErrorDetails>> handlePackageNotFoundException(ClassCastException ex, WebRequest request) {
+    public final ResponseEntity<AppResponse<ErrorDetails>> handleClassCastException(ClassCastException ex, WebRequest request) {
 
         ErrorDetails errorDetails =
                 new ErrorDetails(new Date(), ex.getMessage(),
                         request.getDescription(true), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        return ResponseEntity.status(errorDetails.getCode()).body(AppResponse.<ErrorDetails>builder()
+                .message(ex.getMessage())
+                .status(errorDetails.getCode().name())
+                .error(errorDetails)
+                .build());
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public final ResponseEntity<AppResponse<ErrorDetails>> HttpClientErrorException(HttpClientErrorException ex, WebRequest request) {
+
+        ErrorDetails errorDetails =
+                new ErrorDetails(new Date(), ex.getMessage(),
+                        request.getDescription(true), HttpStatus.NOT_FOUND);
 
         return ResponseEntity.status(errorDetails.getCode()).body(AppResponse.<ErrorDetails>builder()
                 .message(ex.getMessage())
